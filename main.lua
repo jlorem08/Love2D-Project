76f5f7deb558d659edbb2838655f77778c023c91
jlorem08 <j.schmidt23@mail.lorainccc.edu>
2025-06-27 07:32:54 -0400

    initial commit

---
+++ main.lua
@@ -0,0 +1,11 @@
+function love.load() -- runs at start, for setup mostly
+
+end
+
+function love.update(dt) -- runs every frame before draw
+
+end
+
+function love.draw() -- runs every frame after update
+
+end

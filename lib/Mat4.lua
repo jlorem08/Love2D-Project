f55964d9efeb1b4ebe4a4f910f7ca5427ead036f
jlorem08 <j.schmidt23@mail.lorainccc.edu>
2025-06-27 21:58:59 -0400

    added mul metamethod

--- lib/Mat4.lua
+++ lib/Mat4.lua
@@ -1,3 +1,6 @@
+---------- IMPORTS ----------
+local Vector3 = require("Vector3")
+
 ---------- SETUP ----------
 
 local Mat4 = {}
@@ -78,7 +81,42 @@ end
 
 ---------- METAMETHODS ----------
 
+function Mat4:__mul(a, b)
+    if getmetatable(a) == Mat4 and getmetatable(b) == Mat4 then
+        local matrixData = {}
 
+        for row = 1, 4 do
+            for column = 1, 4 do
+                local sum = 0
+
+                for i = 1, 4 do
+                    sum = sum + a:get(row, i) * b:get(i, column)
+                end
+
+                matrixData[(row - 1) * 4 + column] = sum
+            end
+        end
+
+        return Mat4.new(matrixData)
+    elseif getmetatable(a) == Mat4 and getmetatable(b) == Vector3 then
+        local x, y, z = b.X, b.Y, b.Z
+        local m = a.data
+
+        local newX = m[1]*x + m[2]*y + m[3]*z + m[4]*1
+        local newY = m[5]*x + m[6]*y + m[7]*z + m[8]*1
+        local newZ = m[9]*x + m[10]*y + m[11]*z + m[12]*1
+        local w = m[13]*x + m[14]*y + m[15]*z + m[16]*1
+
+        -- normalize if w is not 1
+        if w ~= 0 and w ~= 1 then
+            newX = newX / w
+            newY = newY / w
+            newZ = newZ / w
+        end
+
+        return Vector3.new(newX, newY, newZ)
+    end
+end
 
 ---------- METHODS ----------
 

f5480dfca2b33471cf59361039db76552abdbf7d
jlorem08 <j.schmidt23@mail.lorainccc.edu>
2025-06-27 23:03:40 -0400

    initial commit

---
+++ engine/Renderer/renderer.lua
@@ -0,0 +1,20 @@
+---------- IMPORTS ----------
+
+local Triangle = require("Renderer.Triangle")
+local Camera = require("Scene.Camera")
+local Scene = require("Scene.Scene")
+local Mat4 = require("lib.Mat4")
+local Vector3 = require("lib.Vector3")
+
+---------- SETUP ----------
+
+local Renderer = {}
+Renderer.__index = Renderer
+
+---------- CONSTRUCTOR ----------
+
+function Renderer.new(screenWidth, screenHeight)
+    
+end
+
+---------- METAMETHODS ----------

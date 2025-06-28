9ee859e965987a5dc5d253132ae5ce981eb468ce
jlorem08 <j.schmidt23@mail.lorainccc.edu>
2025-06-27 21:26:51 -0400

    added new constructors

--- lib/Mat4.lua
+++ lib/Mat4.lua
@@ -18,12 +18,26 @@ function Mat4.new(data)
     return self
 end
 
-function Mat4:get(row, column)
-    return self.data[(row - 1) * 4 + column]
+function Mat4.identity()
+    return Mat4.new()
 end
 
-function Mat4:set(row, column, value)
-    self.data[(row - 1) * 4 + column] = value
+function Mat4.translation(x, y, z)
+    return Mat4.new({
+        1, 0, 0, x,
+        0, 1, 0, y,
+        0, 0, 1, z,
+        0, 0, 0, 1,
+    })
+end
+
+function Mat4.scale(x, y, z)
+    return Mat4.new({
+        x, 0, 0, 0,
+        0, y, 0, 0,
+        0, 0, z, 0,
+        0, 0, 0, 1,
+    })
 end
 
 ---------- METAMETHODS ----------
@@ -32,7 +46,13 @@ end
 
 ---------- METHODS ----------
 
+function Mat4:get(row, column)
+    return self.data[(row - 1) * 4 + column]
+end
 
+function Mat4:set(row, column, value)
+    self.data[(row - 1) * 4 + column] = value
+end
 
 ---------- CLOSING ----------
 

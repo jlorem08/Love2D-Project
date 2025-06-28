e6169d985e2a48b348e54a54547ea320808d49fb
jlorem08 <j.schmidt23@mail.lorainccc.edu>
2025-06-27 22:09:46 -0400

    added constructor and transform method

---
+++ engine/Renderer/Triangle.lua
@@ -0,0 +1,38 @@
+---------- IMPORTS ----------
+local Vector3 = require("Vector3")
+local Mat4 = require("lib.Mat4")
+
+---------- SETUP ----------
+
+local Triangle = {}
+Triangle.__index = Triangle
+
+---------- CONSTRUCTOR ----------
+
+function Triangle.new(vertex1Vector, vertex2Vector, vertex3Vector)
+    local self = setmetatable({}, Triangle)
+
+    self.v1 = vertex1Vector
+    self.v2 = vertex2Vector
+    self.v3 = vertex3Vector
+
+    return self
+end
+
+---------- METAMETHODS ----------
+
+
+
+---------- METHODS ----------
+
+function Triangle:transform(matrix)
+    return Triangle.new(
+        matrix * self.v1,
+        matrix * self.v2,
+        matrix * self.v3
+    )
+end
+
+---------- CLOSING ----------
+
+return Triangle

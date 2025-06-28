1bebdb39379b3635371d8cff1319fc8250f0296d
jlorem08 <j.schmidt23@mail.lorainccc.edu>
2025-06-27 22:35:08 -0400

    added some basic methods

--- engine/Scene/scene.lua
+++ engine/Scene/scene.lua
@@ -25,7 +25,27 @@ end
 
 ---------- METHODS ----------
 
+function Scene:addObject(obj)
+    table.insert(self.objects, obj)
+end
 
+function Scene:setCamera(camera)
+    self.camera = camera
+end
+
+function Scene:update(dt)
+    for _, obj in ipairs(self.objects) do
+        if obj.update then
+            obj:update(dt)
+        end
+    end
+end
+
+function Scene:render(renderer)
+    for _, obj in ipairs(self.objects) do
+        renderer:draw(obj, self.camera)
+    end
+end
 
 ---------- CLOSING ----------
 

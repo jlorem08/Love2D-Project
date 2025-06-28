5d6d15fa5f95f75965350ce5b29f19653d0960de
jlorem08 <j.schmidt23@mail.lorainccc.edu>
2025-06-27 21:22:15 -0400

    added constructor, get method, and set method

--- lib/Mat4.lua
+++ lib/Mat4.lua
@@ -5,7 +5,26 @@ Mat4.__index = Mat4
 
 ---------- CONSTRUCTOR ----------
 
+function Mat4.new(data)
+    local self = setmetatable({}, Mat4)
 
+    self.data = data or { -- set to given matrix or identity matrix
+        1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1,
+    }
+
+    return self
+end
+
+function Mat4:get(row, column)
+    return self.data[(row - 1) * 4 + column]
+end
+
+function Mat4:set(row, column, value)
+    self.data[(row - 1) * 4 + column] = value
+end
 
 ---------- METAMETHODS ----------
 

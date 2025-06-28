0ec1810c03d8b1edfa3f690748e20cb2b6f6f63c
jlorem08 <j.schmidt23@mail.lorainccc.edu>
2025-06-27 22:30:17 -0400

    initial commit

---
+++ engine/Scene/scene.lua
@@ -0,0 +1,30 @@
+---------- IMPORTS ----------
+local Vector3 = require("Vector3")
+local Mat4 = require("lib.Mat4")
+
+---------- SETUP ----------
+
+local Setup = {}
+Setup.__index = Setup
+
+---------- CONSTRUCTOR ----------
+
+function Setup.new(vertex1Vector, vertex2Vector, vertex3Vector)
+    local self = setmetatable({}, Setup)
+
+    
+
+    return self
+end
+
+---------- METAMETHODS ----------
+
+
+
+---------- METHODS ----------
+
+
+
+---------- CLOSING ----------
+
+return Setup
